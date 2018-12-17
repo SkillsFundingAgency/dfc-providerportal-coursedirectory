f5f34da4858db2ab9aaf55d95ea27dbd0828a016
ash-h <a.henry@kuumba.co.uk>
2018-12-17 08:52:46 +0000

    Add contents oage | AH

--- src/Dfc.ProviderPortal.CourseDirectory.Web/Startup.cs
+++ src/Dfc.ProviderPortal.CourseDirectory.Web/Startup.cs
@@ -42,7 +42,7 @@ namespace Dfc.ProviderPortal.CourseDirectory.Web
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Providers}/{action=Index}/{id?}");
+                    template: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
