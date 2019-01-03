62bfacac38b8bf3aadaa06960c6fe100cfed0faf
ash-h <a.henry@kuumba.co.uk>
2019-01-03 07:33:13 +0000

    search provider | add no results | AH

--- src/Dfc.ProviderPortal.CourseDirectory.Web/Controllers/ProvidersController.cs
+++ src/Dfc.ProviderPortal.CourseDirectory.Web/Controllers/ProvidersController.cs
@@ -32,6 +32,10 @@ namespace Dfc.ProviderPortal.CourseDirectory.Web.Controllers
         {
             return View();
         }
+        public IActionResult NoResults()
+        {
+            return View();
+        }
         public IActionResult Details()
         {
             return View();
