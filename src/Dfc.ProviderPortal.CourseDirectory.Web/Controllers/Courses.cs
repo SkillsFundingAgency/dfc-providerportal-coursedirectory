107c13b2f46e818cec3ca06c332c6adfe668a95c
ash-h <a.henry@kuumba.co.uk>
2019-01-30 16:31:32 +0000

    courses | AH

---
+++ src/Dfc.ProviderPortal.CourseDirectory.Web/Controllers/Courses.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dfc.ProviderPortal.CourseDirectory.Web.Controllers
+{
+    public class Courses : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Advertise()
+        {
+            return View();
+        }
+
+        public IActionResult Summary()
+        {
+            return View();
+        }
+        public IActionResult MakeChanges()
+        {
+            return View();
+        }
+        public IActionResult ViewAll()
+        {
+            return View();
+        }
+        public IActionResult Confirmation()
+        {
+            return View();
+        }
+        public IActionResult Archive()
+        {
+            return View();
+        }
+
+    }
+}
