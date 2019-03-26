176aab5b98156366007007e0ff49afad07a0f032
ash-h <a.henry@kuumba.co.uk>
2019-03-26 08:48:45 +0000

    yc and non reg changes

--- src/Dfc.ProviderPortal.CourseDirectory.Web/wwwroot/dist/site.js
+++ src/Dfc.ProviderPortal.CourseDirectory.Web/wwwroot/dist/site.js
@@ -1,3 +1,35 @@
+
+
+
+const item = document.getElementsByClassName('item');
+const items = [...item];
+
+
+for(let x of items) {
+    items.forEach( (item) => {
+        //item.classList.remove('active'); 
+console.log(item.classList);
+    })
+    x.addEventListener('click', () => {
+
+
+        //if (x.classList.contains('active')) {
+        //    x.classList.remove('active');
+        //}
+    x.classList.add('active');
+items.forEach( (val) => {
+    if (val.classList.contains('active')) {
+    console.log(val.classList);
+}
+});
+
+//if (x)
+  //      console.log( ...x.childNodes);
+    })
+}
+
+
+
 /* eslint-disable no-console */
 // jQuery Validate Hooks
 
