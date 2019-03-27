44dc4b189749a8c7874d5f3f8fa51553c549ed8d
ash-h <a.henry@kuumba.co.uk>
2019-03-27 10:12:25 +0000

    non reg start

--- src/Dfc.ProviderPortal.CourseDirectory.Web/Controllers/LarsController.cs
+++ src/Dfc.ProviderPortal.CourseDirectory.Web/Controllers/LarsController.cs
@@ -31,5 +31,9 @@ namespace Dfc.ProviderPortal.CourseDirectory.Web.Controllers
         {
             return View();
         }
+        public IActionResult Home()
+        {
+            return View();
+        }
     }
 }
