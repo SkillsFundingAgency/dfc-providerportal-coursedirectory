b7b5f3a42937a8492c40997148d9f0361ba25b51
David Olsan <davidolsan@davidolsan.com>
2019-04-12 10:31:40 +0100

    ur journey

---
+++ src/Dfc.ProviderPortal.CourseDirectory.Web/Controllers/ur.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dfc.ProviderPortal.CourseDirectory.Web.Controllers
+{
+    public class UR : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        public IActionResult CoursePage()
+        {
+            return View();
+        }
+   
+
+        public IActionResult CourseList()
+        {
+            return View();
+        }
+
+        public IActionResult EditCourse()
+        {
+            return View();
+        }
+
+    }
+}
