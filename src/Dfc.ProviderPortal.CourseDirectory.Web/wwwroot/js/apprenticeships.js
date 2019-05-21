4d48189f2a35d8ac90af5a30ecdc651e9d0f37ff
ash-h <a.henry@kuumba.co.uk>
2019-05-21 11:25:26 +0100

    update apprenticeships add journeys

--- src/Dfc.ProviderPortal.CourseDirectory.Web/wwwroot/js/apprenticeships.js
+++ src/Dfc.ProviderPortal.CourseDirectory.Web/wwwroot/js/apprenticeships.js
@@ -29,7 +29,8 @@ var target = document.getElementsByClassName('cd-apprenticeships-results')[0];
         <h3 class="govuk-heading-m" >${x.LearnAimRefTitle}</h3>
         <p class="govuk-body">Level: <span class="govuk-!-font-weight-bold">${x.NotionalNVQLevelv2}</span></p>
         <p class="govuk-body"><a class="govuk-link" href="/apprenticeships/add">Choose this apprenticeship</a></p>
-     </div>`
+    </div>`
+
       courses.push(course);
     }
     courses.forEach((course)=> {
@@ -66,3 +67,56 @@ if (apprenticeshipQuestion) {
 
 radioSwitcher('apprenticeship-type');
 radioSwitcher('employerNationalQuestion');
+
+
+const DualDeliveryPage = document.getElementsByClassName('dual-delivery')[0];
+
+if (DualDeliveryPage) {
+  
+    let employerCheckbox = document.getElementById('employer');
+    let selectOption = document.querySelectorAll('.select-list')[0];
+
+    employerCheckbox.addEventListener("click", () => {
+        selectOption.classList.toggle("visible");
+    })
+   
+}
+
+const checkBoxes = document.querySelectorAll('.govuk-checkboxes__input');
+
+let options = [];
+ 
+function getOptions () {
+    let x = ``;
+checkBoxes.forEach((checkbox) => {
+               
+        if (checkbox.checked == true) {
+
+            options.push(checkbox.value);
+           let uniqueOptions = Array.from(new Set(options));
+
+            let row = ` <tr class="govuk-table__row">
+                    <th class="govuk-table__header" scope="row">venue</th>
+                    <td class="govuk-table__cell govuk-table__cell--numeric">${uniqueOptions}</td>
+                    <td class="govuk-table__cell govuk-table__cell--numeric"><a href="#">delete</a></td>
+                </tr>`;
+            x = row;
+
+           
+        }
+
+});
+    return x;
+}
+
+
+
+let addAnother = document.getElementById('List');
+
+addAnother.addEventListener('click', () => {
+    let v = getOptions();
+    let target = document.getElementById("Results");
+    target.innerHTML +=(v);
+    let table = document.getElementById('ResultsTable');
+    table.classList.add('visible-table');
+})
