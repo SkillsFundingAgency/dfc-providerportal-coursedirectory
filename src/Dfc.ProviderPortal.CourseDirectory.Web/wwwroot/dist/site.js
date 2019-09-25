d3c1c87a97eabeadd3d637557635afdf4f48a803
ash-h <a.henry@kuumba.co.uk>
2019-09-25 14:12:39 +0100

    add both apprenticeships inputs

--- src/Dfc.ProviderPortal.CourseDirectory.Web/wwwroot/dist/site.js
+++ src/Dfc.ProviderPortal.CourseDirectory.Web/wwwroot/dist/site.js
@@ -80,7 +80,7 @@ if (apprenticeshipQuestion) {
 
 radioSwitcher('apprenticeship-type');
 radioSwitcher('employerNationalQuestion');
-radioSwitcher('employerNationalQuestion');
+
 
 
 const DualDeliveryPage = document.getElementsByClassName('dual-delivery')[0];
